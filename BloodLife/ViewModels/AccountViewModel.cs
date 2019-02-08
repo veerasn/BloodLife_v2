174a2db54fd77d9f202a0d4c8278e00fe2be03ba
Veera Sekaran Nadarajan <veera.nadarajan@gmail.com>
2019-02-08 17:28:15 +0800

    Added Admin controller, view and ViewModel

---
+++ BloodLife/ViewModels/AccountViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using BloodLife.ViewModels;
+using BloodLife.Models;
+
+
+namespace BloodLife.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email address is required")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [StringLength(100, ErrorMessage = "Minimum {2} and Maximum {1} characters are allowed", MinimumLength = 5)]
+        public string UserEmailId { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, ErrorMessage = "Minimum {2} and Maximum {1} characters are allowed", MinimumLength = 5)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+
+        public int UserType { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Email address is required")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [StringLength(100, ErrorMessage = "Minimum {2} and Maximum {1} characters are allowed", MinimumLength = 5)]
+        [System.Web.Mvc.Remote("CheckEmailExist", "Account", ErrorMessage = "Email address is already used")]
+        public string UserEmailId { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, ErrorMessage = "Minimum {2} and Maximum {1} characters are allowed", MinimumLength = 5)]
+        public string Password { get; set; }
+
+        [Compare("NewPassword", ErrorMessage = "Password doesn't match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "User type")]
+        public int UserType { get; set; }
+
+        public bool TermsAndConditions { get; set; }
+    }
+
+    public class ChangePasswordViewModel
+    {
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "Minimum {2} and Maximum {1} characters are allowed", MinimumLength = 5)]
+        public string OldPassword { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, ErrorMessage = "Minimum {2} and Maximum {1} characters are allowed", MinimumLength = 5)]
+        public string NewPassword { get; set; }
+
+        [Compare("NewPassword", ErrorMessage = "Password doesn't match")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class ForgotPasswordViewModel
+    {
+        [Required(ErrorMessage = "Email address is required")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [StringLength(100, ErrorMessage = "Minimum {2} and Maximum {1} characters are allowed", MinimumLength = 5)]
+        public string EmailId { get; set; }
+    }
+
+    public class ResetPasswordViewModel
+    {
+        public string EmailId { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, ErrorMessage = "Minimum {2} and Maximum {1} characters are allowed", MinimumLength = 5)]
+        public string NewPassword { get; set; }
+
+        [Compare("NewPassword", ErrorMessage = "Password doesn't match")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class AccountViewModel
+    {
+    }
+}
